f34ce3ff68ac688d070c5639b171d2f5bfd71ded
hardtanh <fcdjin@hardtanh.com>
2026-05-11 19:16:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## install
+```bash
+mkdir torch-tutorials
+cd torch-tutorials
+
+uv init --python 3.12
+
+uv pip install torch=="2.2.0" torchvision torchaudio
+uv add "numpy<2"
+
+uv run python -c "import torch; print(torch.backends.mps.is_available())"
+```
---
+++ gpu_cpu.py
@@ -0,0 +1,18 @@
+import torch 
+
+if __name__ == "__main__":
+        # Example tensor operation to test GPU
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        x = torch.rand(10000, 10000, device=device)
+        y = torch.rand(10000, 10000, device=device)
+        z = x + y
+        print("Tensor operation successful on GPU.")
+    else:
+        print("CUDA is not available. Running on CPU.")
+        device = torch.device("cpu")
+        x = torch.rand(10000, 10000, device=device)
+        y = torch.rand(10000, 10000, device=device)
+        z = x + y
+        print("Tensor operation successful on CPU.")
+        print(f"results {z}")

f92c59eb263ca97c213c1677185a137c665dd5a5
hardtanh <fcdjin@hardtanh.com>
2026-05-11 19:21:55 -0500

    initial commit

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import torch
 
 if __name__ == "__main__":
     print("PyTorch version:", torch.__version__)
-    print("Is MPS available?", torch.backends.mps.is_available())
     print("GPU count:", torch.cuda.device_count())
     print("CUDA available?", torch.cuda.is_available())
     # print("Current device:", torch.cuda.current_device())
+    # print("Is MPS available?", torch.backends.mps.is_available())
     # print("Device name:", torch.cuda.get_device_name(torch.cuda.current_device()))
     
